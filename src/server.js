3d702dda19d39b76c913974c476e83bb9394f12b
Nick Richard <nickrichard@Nicks-Mac-mini.local>
2019-08-30 17:15:49 -0500

    first commit

--- src/server.js
+++ src/server.js
@@ -6,14 +6,26 @@ const path = require('path');
 const dns = require('dns');
 const { MongoClient } = require('mongodb');
 const nanoid = require('nanoid');
+var cors = require('cors');
 
 const databaseUrl = process.env.DATABASE;
+
+console.log(databaseUrl)
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(cors());
+
+// app.all("/*", function(req, res, next){
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//   next();
+// });
+
 
 MongoClient.connect(databaseUrl, { useNewUrlParser: true })
   .then(client => {
@@ -40,15 +52,16 @@ const shortenURL = (db, url) => {
 const checkIfShortIdExists = (db, code) => db.collection('shortenedURLs')
   .findOne({ short_id: code });
 
-app.get('/', (req, res) => {
-  const htmlPath = path.join(__dirname, 'public', 'index.html');
-  res.sendFile(htmlPath);
-})
+// app.get('/', (req, res) => {
+//   const htmlPath = path.join(__dirname, 'public', 'index.html');
+//   res.sendFile(htmlPath);
+// })
 
 app.post('/new', (req, res) => {
   let originalUrl;
   try {
     originalUrl = new URL(req.body.url);
+    console.log(originalUrl)
   } catch (err) {
     return res.status(400).send({error: 'invalid URL'});
   }
@@ -66,6 +79,10 @@ app.post('/new', (req, res) => {
           original_url: doc.original_url,
           short_id: doc.short_id,
         });
+        
+        // short url w/o domain prefix
+        console.log(doc.short_id)
+
       })
       .catch(console.error);
   });
@@ -89,3 +106,44 @@ app.set('port', process.env.PORT || 4100);
 const server = app.listen(app.get('port'), () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
+
+app.post('/shortenUrl', function(req, res){
+  let shortURL;
+  
+  console.log(req.body.url);
+  let originalUrl;
+  try {
+    originalUrl = new URL(req.body.url);
+    console.log(originalUrl)
+  } catch (err) {
+    return res.status(400).send({error: 'invalid URL'});
+  }
+
+  dns.lookup(originalUrl.hostname, (err) => {
+    if (err) {
+      return res.status(404).send({error: 'Address not found'});
+    };
+
+    const { db } = req.app.locals;
+    shortenURL(db, originalUrl.href)
+      .then(result => {
+        const doc = result.value;
+        console.log(doc.short_id)
+        res.format ({
+          'application/json': function() {
+            res.send({ message: doc.short_id });
+         }})
+        
+        // res.json({
+        //   original_url: doc.original_url,
+        //   short_id: doc.short_id,
+        // });
+        
+        // // short url w/o domain prefix
+
+        // res.send(doc.short_id);
+      })
+      .catch(console.error);
+  });
+  
+})
